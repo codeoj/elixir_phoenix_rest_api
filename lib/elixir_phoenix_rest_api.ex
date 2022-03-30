94d63107bce40080c3cd8f68c28f10d5d89f5d24
Oliver <oliverjgodoy@gmail.com>
2022-03-30 19:55:51 -0300

    created delete user flow, and your validations

--- lib/elixir_phoenix_rest_api.ex
+++ lib/elixir_phoenix_rest_api.ex
@@ -2,4 +2,5 @@ defmodule ElixirPhoenixRestApi do
   alias ElixirPhoenixRestApi.User
 
   defdelegate create_user(params), to: User.Create, as: :call
+  defdelegate delete_user(params), to: User.Delete, as: :call
 end
---
+++ lib/elixir_phoenix_rest_api/user/delete.ex
@@ -0,0 +1,20 @@
+defmodule ElixirPhoenixRestApi.User.Delete do
+  alias ElixirPhoenixRestApi.{User, Repo}
+  alias Ecto.UUID
+
+  def call(id) do
+    case UUID.cast(id) do
+      :error -> {:error, "Invalid ID"}
+      {:ok, uuid} -> delete(uuid)
+    end
+  end
+
+  defp delete(uuid) do
+    case fetch_user(uuid) do
+      nil -> {:erro, "User does not exist"}
+      user -> Repo.delete(user)
+    end
+  end
+
+  defp fetch_user(uuid), do: Repo.get(User, uuid)
+end
